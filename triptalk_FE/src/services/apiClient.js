32ea2da863a20e074fa9eefe1f087e7c2c13b391
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2026-07-19 18:08:29 +0900

    baseURL 수정

--- triptalk_FE/src/services/apiClient.js
+++ triptalk_FE/src/services/apiClient.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 export const apiClient = axios.create({
-  // baseURL: 'https://triptalk-backend.onrender.com',
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://triptalk-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
